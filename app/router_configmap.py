fba4e015bb671cd204fd9d5b55b315e2aec12aa3
Constantin Radulescu <constatin.r@gmail.com>
2023-02-04 23:41:14 +0200

    Complete CRUD for configmap

--- app/router_configmap.py
+++ app/router_configmap.py
@@ -1,8 +1,45 @@
+
+from typing import Dict
 from fastapi import HTTPException, APIRouter
 from kubernetes import client
-from starlette.responses import JSONResponse
 
 router = APIRouter()
+
+
+@router.post("/api/v1/namespaces/{namespace}/configmaps")
+def create_configmap_in_given_namespace_api(
+        namespace: str,
+        name: str,
+        data: Dict[str, str]
+):
+    try:
+        # Create the ConfigMap object
+        configmap = client.V1ConfigMap(
+            metadata=client.V1ObjectMeta(
+                name=name,
+                namespace=namespace
+            ),
+            data=data
+        )
+
+        # Create the ConfigMap
+        v1_core_api = client.CoreV1Api()
+        v1_core_api.create_namespaced_config_map(
+            namespace=namespace,
+            body=configmap
+        )
+        results = []
+        configmap = v1_core_api.read_namespaced_config_map(name=name, namespace=namespace)
+        configmap_info = {
+            "name": configmap.metadata.name,
+            "namespace": configmap.metadata.namespace,
+            "data": configmap.data,
+            "creationTimestamp": configmap.metadata.creationTimestamp
+        }
+        results.append(configmap_info)
+        return results
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/api/v1/get-all-configmaps-in-cluster")
@@ -12,10 +49,55 @@ def get_all_from_cluster_api():
     results = []
     try:
         for configmap in configmaps.items:
-            configmap_data = configmap.to_dict()
-            configmap_data.pop("metadata", {})
-            results.append(configmap_data)
+            configmap_info = {
+                "name": configmap.metadata.name,
+                "namespace": configmap.metadata.namespace,
+                "data": configmap.data
+            }
+            results.append(configmap_info)
         return {"configmaps": results}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/api/v1/get-{namespace}/configmap{name}")
+def get_configmap_from_given_namespace_api(
+        name: str,
+        namespace: str
+):
+    v1_core_api = client.CoreV1Api()
+
+    try:
+        results = []
+        configmap = v1_core_api.read_namespaced_config_map(name=name, namespace=namespace)
+        configmap_info = {
+            "name": configmap.metadata.name,
+            "namespace": configmap.metadata.namespace,
+            "data": configmap.data
+        }
+        results.append(configmap_info)
+        return {"configmap": results}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/api/v1/list-configmaps/{namespace}")
+def list_configmap_from_given_namespace_api(
+        namespace: str
+):
+    v1_core_api = client.CoreV1Api()
+
+    try:
+        results = []
+        configmaps = v1_core_api.list_namespaced_config_map(namespace=namespace)
+        for configmap in configmaps.items:
+            configmap_info = {
+                "name": configmap.metadata.name,
+                "namespace": configmap.metadata.namespace,
+                "data": configmap.data
+            }
+            results.append(configmap_info)
+        return {"configmap": results}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
