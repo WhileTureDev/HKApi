ebe637c7f94c01472c4557e6b585c0dd86019b43
Constantin Radulescu <constatin.r@gmail.com>
2023-02-04 21:21:15 +0200

    Add edit for secrets and configmaps

--- app/router_edit_object.py
+++ app/router_edit_object.py
@@ -4,8 +4,8 @@ from kubernetes import client
 router = APIRouter()
 
 
-@router.put("/api/v1/namespaces/{namespace}/deployments/{name}")
-async def edit_deployment(
+@router.put("/api/v1/edit/{namespace}/deployments/{name}")
+async def edit_deployment_api(
         name: str,
         namespace: str,
         replicas: int = None,
@@ -30,5 +30,59 @@ async def edit_deployment(
         )
 
         return {"message": f"Deployment '{name}' updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.put("/api/v1/edit/{namespace}/secrets/{name}")
+async def edit_secret_api(
+        name: str,
+        namespace: str,
+        data: dict = None
+):
+    try:
+        # Get the Secret
+        core_v1_api = client.CoreV1Api()
+        secret = core_v1_api.read_namespaced_secret(name=name, namespace=namespace)
+
+        # Edit the Secret
+        if data:
+            secret.data = data
+
+        # Update the Secret
+        core_v1_api.patch_namespaced_secret(
+            name=name,
+            namespace=namespace,
+            body=secret
+        )
+
+        return {"message": f"Secret '{name}' updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.put("/api/v1/edit/{namespace}/configmaps/{name}")
+async def edit_config_map_api(
+        name: str,
+        namespace: str,
+        data: dict = None
+):
+    try:
+        # Get the ConfigMap
+        core_v1_api = client.CoreV1Api()
+        config_map = core_v1_api.read_namespaced_config_map(name=name, namespace=namespace)
+
+        # Edit the ConfigMap
+        if data:
+            config_map.data = data
+
+        # Update the ConfigMap
+        core_v1_api.patch_namespaced_config_map(
+            name=name,
+            namespace=namespace,
+            body=config_map
+        )
+
+        return {"message": f"ConfigMap '{name}' updated successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
