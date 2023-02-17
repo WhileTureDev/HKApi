dd1f164862d08e6fadddba202231951e62c566cb
Constantin Radulescu <constatin.r@gmail.com>
2023-02-17 12:17:12 +0200

    Add register and login endpoint

--- app/db.py
+++ app/db.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from sqlalchemy import create_engine, Column, String, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import sqlalchemy.orm as _orm
 
 psq_pass = os.getenv("POSTGRES_PASSWORD")
 Base = declarative_base()
@@ -13,11 +14,14 @@ class Deployment(Base):
     __tablename__ = 'deployments'
 
     id = Column(Integer, primary_key=True, index=True)
+    release_type = Column(String, index=True)
+    install_type = Column(String, index=True)
     chart_name = Column(String, index=True)
     chart_repo_url = Column(String, index=True)
     namespace = Column(String, index=True)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow)
+    owner = _orm.relationship("User", back_populates="deployments")
 
 
 class User(Base):
@@ -29,6 +33,7 @@ class User(Base):
     password = Column(String)
     created_at = Column(DateTime)
     updated_at = Column(DateTime)
+    deployments = _orm.relationship("Deployment", back_populates="owner")
 
 
 # Connect to the database
@@ -55,6 +60,7 @@ def create_deployment_record(chart_name, chart_repo_url, namespace):
         exist_namespace = Deployment(chart_name=chart_name,
                                      chart_repo_url=chart_repo_url,
                                      namespace=namespace,
+                                     install_type="new",
                                      created_at=datetime.utcnow())
         ns.append(exist_namespace)
         session.add(exist_namespace)
@@ -64,6 +70,7 @@ def create_deployment_record(chart_name, chart_repo_url, namespace):
         new_namespace = Deployment(chart_name=chart_name,
                                    chart_repo_url=chart_repo_url,
                                    namespace=namespace,
+                                   install_type="update",
                                    updated_at=datetime.utcnow())
         ns.append(new_namespace)
         session.add(new_namespace)
