480953faaedf5d9e36afea11bd6c3091950d6c5a
cradulescu <constatin.r@gmail.com>
2024-06-14 02:02:15 +0000

    [Frontend] - Adjust Header.tsx

--- frontend/hkui/app/lib/Header.tsx
+++ frontend/hkui/app/lib/Header.tsx
@@ -2,13 +2,15 @@
 
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import styles from '@/app/styles/Api.module.css';
+import styles from '@/app/styles/shared.module.css';
+import userIcon from './img/user-icon-2048x2048-ihoxz4vq.png'; // Import the image
 
 const Header: React.FC = () => {
     const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
     const router = useRouter();
 
     const handleLogout = () => {
@@ -16,11 +18,33 @@ const Header: React.FC = () => {
         router.push('/');
     };
 
+    const toggleDropdown = () => {
+        setIsDropdownVisible(!isDropdownVisible);
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            setIsDropdownVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        if (isDropdownVisible) {
+            document.addEventListener('click', handleClickOutside);
+        } else {
+            document.removeEventListener('click', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [isDropdownVisible]);
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>HKUI</div>
-            <div className={styles.userProfile} onClick={() => setIsDropdownVisible(!isDropdownVisible)}>
-                <img src="/profile.png" alt="User Profile" />
+            <div className={styles.userProfile} onClick={toggleDropdown} ref={dropdownRef}>
+                <img src={userIcon.src} alt="User Profile" className={styles.userIcon} /> {/* Use the imported image */}
                 {isDropdownVisible && (
                     <div className={styles.dropdownMenu}>
                         <Link href="/profile">Profile</Link>
